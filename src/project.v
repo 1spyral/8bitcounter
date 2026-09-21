7e58fbcb74936be7c0f5b8048723c37c45a9b4ee
Luke Zhan <luke.zhan@uwaterloo.ca>
2026-09-21 18:47:02 -0400

    Implement counter
    Implement counter
    
    Update project.v
    
    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2026 Luke Zhan
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_1spyral (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,23 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+  reg [7:0] count;
+
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+      count <= '0;
+    else if (uio_in[0])
+      count <= ui_in;
+    else
+      count <= count + 1;
+  end
+
+  assign uo_out = uio_in[1] ? count : 8'bz;
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in[7:2], 1'b0};
 
 endmodule
